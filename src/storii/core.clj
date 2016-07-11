c8f99542e73d3cbd7a267ff3f44f39f76cfb6f32
nateboxer <nate@nateboxer.net>
2016-07-11 15:28:28 -0700

    some help and exit

--- src/storii/core.clj
+++ src/storii/core.clj
@@ -1,14 +1,31 @@
 (ns storii.core
   (:gen-class))
 
+(defn show-stories []
+  (println "no stories :(")
+)
+
+(defn show-help []
+  (println "help - this help")
+  (println "list - list stories")
+  (println "open [story name] - open a story")
+  (println "exit | quit - quit Storii")
+)
+
 (defn handle-input [input]
-  input)
+  (when (= input "help")
+    (show-help))
+  (when (= input "list")
+    (show-stories))
+  (when (contains? #{"exit" "quit"} input)
+    (System/exit 0))
+)
 
 (defn start-storii []
   (while true
     (print "> ")
     (flush)
-    (println (handle-input (read-line)))))
+    (handle-input (read-line))))
 
 (defn -main
   [& args]
